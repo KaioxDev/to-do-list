83495fcece6c4e31f09d0d74fb997259d7387008
KaioxDev <ikicicvt@gmail.com>
2024-09-20 18:18:43 -0300

    Aula - Dominando-o-NodeJS-(legado)

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-const express = require('express');
+const express = require("express");
+const checklistRoutes = require("./src/routes/checklist");
+
+const app = express();
+app.use(express.json());
+
+app.use('/checklists', checklistRoutes);
+
+app.listen(3000, () => {
+  console.log("Servidor foi iniciado na porta 3000");
+});
